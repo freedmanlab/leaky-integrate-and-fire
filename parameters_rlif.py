a31da935cbf0ca4c2fab4fe7e8246575f817a471
Adam Fine <afine2@uchicago.edu>
2020-09-15 11:22:48 -0500

    Inhib/excitatory neurons, various other changes
    "Neuronal spike-rate adaptation supports working memory in language processing" might not be a good paper but it has good ideas for the network

--- parameters_rlif.py
+++ parameters_rlif.py
@@ -12,6 +12,9 @@ Independent parameters
 def default_exc_func(V_rest, V_th, tau_ref, gain, V_m, spikes, I, exc):
     return V_rest, V_th, tau_ref, gain
 
+def spikes_to_current(time, tau):
+    return .0101*(np.exp(-(time/(4*tau)))-np.exp(-(time/tau)))
+
 par = {
     # Setup parameters
     'save_dir'              : './savedir/',
@@ -19,30 +22,36 @@ par = {
 
     ##################
     # Leaky intergrate and fire parameters
-    'T'                     : 500.0,       # total time to simulate (ms)
+    'T'                     : 300.0,       # total time to simulate (ms)
     'simulation_dt'         : 0.125,   # Simulation timestep (ms)
-    'V_in'                  : .1,      # Neuron input voltage
-    'input_duration'        : 10.0,     # duration of the input current (ms)
+    'V_in'                  : .05,      # Neuron input voltage
+    'input_duration'        : 8.0,     # duration of the input current (ms)
 
     'gain'                  : 1.0,      # neuron gain (unitless)
     't_rest'                : 0.05,        # initial refractory time
-    't_initial'             : 0.0,        # initial time
     'Rm'                    : 1.0,        # Resistance (kOhm)
     'Cm'                    : 10,       # Capacitance (uF)
-    'tau_ref'               : 10.0,        # refractory period (ms)
+    'tau_ref'               : 5.0,        # refractory period (ms)
+    'tau_abs_ref'           : 1.0,        # absolute refractory period (ms), lower bound for exc_refrac
     'V_th'                  : -.055,     # : 1  #spike threshold
     'V_spike'               : .04,        # spike delta (V)
     'V_rest'                : -.07,        # resting potential (V)
     'type'                  : 'Leaky Integrate and Fire',
     'debug'                 : False,    # Watch neurons get made
     'exc_func'              : default_exc_func, #excitability function
+    'input_stdev'           : .001,     #standard deviation of the input Gaussian noise
+    'voltage_stdev'         : .0008,     #standard deviation of the neuron voltage update, also Gaussian
+    'spikes_to_current_func': spikes_to_current, #function for converting spikes to current
+    'voltage_decay_const'   : .5,        #decay constant for the conversion from spikes to current (ms)
+    'decay_thresh'          : .0005,    #threshold for zeroing the current from a spike
     # Network shape
     'num_layers'            : 1,
     'num_neurons'           : 100,
     'num_inputs'            : 5,
+    'exc_prop'              : .8,     #proportion of neurons that are excitatory
     'input_connect_binary_prob': .3, #governs what fraction of neurons are connected to the inputs
-    'input_connect_frac'    : .5, #governs how many inputs neurons that are connected to the inputs are connected to
-    'recur_connect_frac'    : .6, #governs connections between neurons
+    'input_connect_frac'    : .3, #governs how many inputs neurons that are connected to the inputs are connected to
+    'recur_connect_frac'    : .3, #governs connections between neurons
 }
 
 def update_parameters(updates):
@@ -59,6 +68,12 @@ def update_dependencies():
     par['timesteps'] = int(par['T'] / par['simulation_dt'])
     par['input_timesteps'] = int(par['input_duration']/par['simulation_dt'])
     par['tau_m'] = par['Rm'] * par['Cm'] # Time constant
+    par['num_current_timesteps'] = compute_current_timesteps()
+    par['exc_num'] = int(par['exc_prop']*par['num_neurons'])
 
+def compute_current_timesteps():
+    times = par['simulation_dt']*np.arange(1000)
+    currents = spikes_to_current(times, par['voltage_decay_const'])
+    return 1000-np.argmax(currents[::-1]>par['decay_thresh'])
 # update_parameters()
 update_dependencies()
--- recurrent_lif.py
+++ recurrent_lif.py
@@ -5,30 +5,42 @@ Contributions from Gregory Grant, Catherine Lee
 
 import numpy as np
 import matplotlib.pyplot as plt
-plt.ioff()
 import pandas as pd
 import random
 import matplotlib
 from tqdm import tqdm
 from parameters_rlif import par, update_dependencies
 
-T           = par['T']    # total time to simulate (msec)
+T           = par['T']    # total time to simulate (ms)
 dt          = par['simulation_dt'] # Simulation timestep
 dts        = par['timesteps']
+current_dts = par['num_current_timesteps']
+current_func = par['spikes_to_current_func']
+current_profile = current_func(dt*np.arange(current_dts),par['voltage_decay_const'])
 input_dts = par['input_timesteps']
 V_in       = par['V_in']   # Neuron input voltage
+input_stdev = par['input_stdev']
+voltage_stdev = par['voltage_stdev']
 
 time_range = np.arange(0, T, dt)
 
 num_layers  = par['num_layers']
 num_neurons = par['num_neurons']
 num_inputs = par['num_inputs']
+num_exc = par['exc_num']
 
-np.random.seed(3)
 
-neuron_input = np.random.normal(0, .005, (num_inputs, dts))
+np.random.seed(333)
+
+#makes some neurons inhibitory
+inhib_idxs = np.random.choice(num_neurons, num_neurons-num_exc, replace = False)+num_inputs
+connections_mult_matrix = np.ones(num_inputs+num_neurons)
+connections_mult_matrix[inhib_idxs] = -5
+
+
+neuron_input = np.random.normal(0, input_stdev, (num_inputs, dts))
 for inpt in np.arange(num_inputs):
-    offset = np.random.randint(0, dts / 10)
+    offset = np.random.randint(dts//10, dts // 3)
     neuron_input[inpt, offset:offset + input_dts] += V_in
 
 
@@ -69,17 +81,20 @@ def plot_spikes(time, V_m, neuron_type, neuron_id = 0):
 #generation of connectivity arrays
 def generate_connections(par):
     connections = np.zeros(num_inputs+num_neurons)
+    input_yn = False
     yn_in = np.random.uniform()
     if yn_in < par["input_connect_binary_prob"]:
+        input_yn = True
         c_probs = np.random.uniform(size=num_inputs)
         for input in np.arange(c_probs.shape[0]):
             if c_probs[input] < par["input_connect_frac"]:
-                connections[input] = np.random.uniform(.9,1.1)
+                connections[input] = np.random.exponential(scale=.5)
     c_probs = np.random.uniform(size=num_neurons)
     for neuron in np.arange(c_probs.shape[0]):
         if c_probs[neuron] < par["recur_connect_frac"]:
-            connections[neuron+num_inputs] = np.random.uniform(.9, 1.1)
-    return connections
+            connections[neuron+num_inputs] = np.random.uniform(.1, 1)
+    connections = connections*connections_mult_matrix
+    return connections, input_yn
 
 # Basic LIF Neuron class
 class LIFNeuron():
@@ -92,7 +107,7 @@ class LIFNeuron():
   # Output (spikes) for the neuron
 
         self.gain     = par['gain']      # neuron gain (unitless)
-        self.t        = par['t_initial']         # initial time
+        self.t        = 0                # initial time
         self.Rm       = par['Rm']        # Resistance (kOhm)
         self.Cm       = par['Cm']        # Capacitance (uF)
         self.tau_m    = par['tau_m']     # Time constant (ms)
@@ -103,9 +118,10 @@ class LIFNeuron():
         self.type     = par['type']
         self.debug    = par['debug']
         self.exc_func = specific_params.get("exc_func", par["exc_func"])
-        self.connections = generate_connections(par)
+        self.connections, self.input_connected = generate_connections(par)
 
         self.input = np.zeros(dts)
+        self.output = np.zeros(dts)
         self.exc      = np.zeros((4,dts))# Resting potential (mV?), threshold (mV?), refractory period (ms), gain (unitless)
         self.V_m      = np.zeros(dts)+self.V_rest    # Neuron potential (mV)
         self.spikes   = np.zeros(dts)
@@ -123,13 +139,18 @@ class LIFNeuron():
         if timestep*dt > self.t_rest:
             specific_input = np.sum(input*self.connections)
             self.input[timestep] = specific_input
-            self.V_m[timestep] = self.V_m[timestep-1] + np.random.normal(0, .005) +\
+            self.V_m[timestep] = self.V_m[timestep-1] + np.random.normal(0, voltage_stdev) +\
                 (-self.V_m[timestep-1] + self.exc[0,timestep-1] + self.exc[3,timestep-1]*specific_input*self.Rm) / self.tau_m * self.dt
             self.exc[:, timestep] = self.exc_func(self.V_rest, self.V_th, self.tau_ref, self.gain,
                                       self.V_m[:timestep], self.spikes[:timestep], self.input[:timestep], self.exc[:,:timestep])
 
             if self.V_m[timestep] >= self.exc[1,timestep]:
                 self.spikes[timestep] += self.V_spike
+                try:
+                    self.output[timestep:timestep + current_dts] += current_profile
+                except:
+                    print("ERROR LELLULULULULU")
+                    self.output[timestep:] += current_profile[:dts - timestep]
                 self.spiketimes.append(timestep*dt)
                 # if i+1 < spikes.shape[0]:
                 #     spikes[i+1] += self.V_spike
@@ -142,13 +163,17 @@ class LIFNeuron():
             self.V_m[timestep] = self.exc[0,timestep]
 
 # define resting excitability function - params are V_rest, V_m, spikes, I, exc
-def exc_func(V_rest, V_th, tau_ref, gain, V_m, spikes, I, exc):
+
+#make a spike rate increase function and a spike rate decrease function I think
+def exc_static_up_func(V_rest, V_th, tau_ref, gain, V_m, spikes, I, exc):
+    #make everything decay over time. rewrite this to be delta property?
+
     integrated_spikes = np.sum(spikes[-500:])
     integrated_current = np.sum(I[-500:])
-    exc_rest = V_rest + integrated_spikes/10
-    exc_thresh = V_th - integrated_current/2500
-    exc_refrac = max(1, tau_ref - integrated_spikes*2.5)
-    exc_gain = gain + integrated_spikes/2
+    exc_rest = V_rest + integrated_current/2000
+    exc_th = max(V_rest+.001, V_th - integrated_spikes/50)
+    exc_refrac = max(par['tau_abs_ref'], tau_ref - integrated_spikes*2.5)
+    exc_gain = gain + integrated_spikes*2.5
     return V_rest, V_th, tau_ref, gain
 
 # Create neuronal array
@@ -163,13 +188,14 @@ def create_neurons(num_layers, num_neurons, debug=False, **specific_params):
         neurons.append(neuron_layer)
     return neurons
 
-neurons = create_neurons(num_layers, num_neurons, debug=False, exc_func = exc_func)
+neurons = create_neurons(num_layers, num_neurons, debug=False, exc_func = exc_static_up_func)
+num_input_connected_neurons = np.sum([neurons[0][neuron].input_connected for neuron in np.arange(num_neurons)])
 
 full_input = np.zeros((num_inputs+num_neurons,dts))
 full_input[:num_inputs, :] = neuron_input
 for timestep in tqdm(np.arange(1,dts)):
     for neuron in np.arange(num_neurons):
-        full_input[num_inputs+neuron, timestep] = neurons[0][neuron].spikes[timestep-1]
+        full_input[num_inputs+neuron, timestep] = neurons[0][neuron].output[timestep-1]
     for neuron in np.arange(num_neurons):
         neurons[0][neuron].spike_generator(full_input[:, timestep], timestep)
 
@@ -177,15 +203,41 @@ full_output = np.zeros((num_neurons, dts))
 for neuron in np.arange(num_neurons):
     full_output[neuron, :] = neurons[0][neuron].V_m
 
-fig, axs = plt.subplots(2,1)
+fig, axs = plt.subplots(2,1, sharex=True)
+plt.get_current_fig_manager().window.showMaximized()
+axs[0].set_xlim([0, T])
 for input_num in np.arange(num_inputs):
     axs[0].plot(time_range, full_input[input_num, :], "b,")
+axs[0].axhline(par['V_th'], color="r")
 for output_num in np.arange(num_neurons):
-    axs[0].plot(time_range, neurons[0][neuron].V_m, "r,")
-axs[1].eventplot([neurons[0][neuron].spiketimes for neuron in np.arange(num_neurons)])
+    axs[0].plot(time_range, neurons[0][output_num].V_m, ",")
+
+neuron_spiketimes = [neurons[0][neuron].spiketimes for neuron in np.arange(num_neurons)]
+neuron_input_connections = [neurons[0][neuron].input_connected for neuron in np.arange(num_neurons)]
+sorted_neuron_spiketimes = [spiketime for spiketime, tf in sorted(zip(neuron_spiketimes, neuron_input_connections), key=lambda neuron: neuron[1], reverse=True)]
+neuron_colors = ["b"] * num_neurons
+neuron_colors[:num_input_connected_neurons] = ["r"]*num_input_connected_neurons
+axs[1].eventplot(sorted_neuron_spiketimes, colors=neuron_colors)
 axs[0].set_title("input")
 axs[1].set_title("output")
 
+fig2, axs2 = plt.subplots(2,1, sharex=True)
+plt.get_current_fig_manager().window.showMaximized()
+for neuron_num in np.arange(num_neurons):
+    input_sum = np.sum(neurons[0][neuron_num].input)
+    print("{0}: {1}".format(neurons[0][neuron_num].input_connected, input_sum))
+    if neurons[0][neuron_num].input_connected:
+        axs2[0].plot(time_range, neurons[0][neuron_num].input, linewidth=1)
+    else:
+        axs2[1].plot(time_range, neurons[0][neuron_num].input, linewidth=1)
+axs2[0].set_title("input connected")
+axs2[1].set_title("input not connected")
+
+fig3, axs3 = plt.subplots(4,1, sharex=True)
+for exc_prop in np.arange(4):
+    axs3[exc_prop].plot(time_range, neurons[0][0].exc[exc_prop,:])
+fig3.suptitle("Excitability properties")
+plt.get_current_fig_manager().window.showMaximized()
 plt.show()
 
 
